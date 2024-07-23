cd8de9c0928668386eece931ac3267c4a23dadcf
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-07-23 22:39:38 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,41 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      danger: "#FF3939",
+      primary: {
+        DEFAULT: "#633CFF",
+        100: "#BEADFF",
+        200: "#EFEBFF",
       },
+      gray: {
+        DEFAULT: "#333333",
+        100: "#737373",
+        200: "#D9D9D9",
+        300: "#FAFAFA",
+      },
+      "button-base": {
+        DEFAULT: "#633CFF",
+        active: "#BEADFF",
+        disabled: "#633CFF",
+        300: "#FAFAFA",
+      },
+    },
+
+    scale: {
+      "90": "0.990",
+      "95": "0.95",
+    },
+
+    borderRadius: {
+      DEFAULT: "8px",
+      none: "0",
+      md: "12px",
+    },
+    boxShadow: {
+      input: "0px 0px 32px 0px rgba(99, 60, 255, 0.25)",
     },
   },
   plugins: [],
