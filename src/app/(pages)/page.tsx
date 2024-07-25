2a4b90d2926e87dfc48a6e72df4b095865410538
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-07-25 09:43:36 +0100

    added links page

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,47 +1,66 @@
-"use client"
+'use client'
 import Image from "next/image";
-
 import heroImage from '../../../public/assets/images/hero-image.svg'
 import { useState } from "react";
-import LinkCard from "../components/LinkCard";
 import Mockup from "../components/Mockup";
+import LinkCard from '../components/LinkCard';
 
 export default function Home() {
-  const [isLinkAdded, setIsLinkAdded] = useState(false)
+  const [cards, setCards] = useState<number[]>([]);
+
+  const addCard = () => {
+    setCards(prevCards => [...prevCards, prevCards.length + 1]);
+  };
+
+  const removeCard = (id: number) => {
+    setCards(prevCards => prevCards.filter(cardId => cardId !== id));
+  };
+
   return (
-    <main className="m-2 min-h-screen p-6 md:grid md:grid-cols-5 gap-5">
+    <main className="sm:m-2 min-h-screen p-3 sm:p-6 md:grid md:grid-cols-5 gap-5">
       <div className="hidden md:flex bg-white items-center justify-center col-span-2 p-10 rounded-md">
-        <Mockup/>
+        <Mockup />
       </div>
 
-      <div className="bg-white p-10 col-span-3 rounded-md">
+      <div className="bg-white p-4 sm:p-10 col-span-3 rounded-md">
         <div>
           <h1 className="font-bold text-2xl text-gray leading-9">Customize your links</h1>
           <p className="text-gray-100 font-normal text-base">Add/edit/remove links below and then share all your profiles with the world!</p>
         </div>
 
-        <button className="w-full border border-primary rounded py-[11px] font-semibold text-primary mt-10 hover:bg-primary-200 hover:border-primary-200 duration-200">+ Add new link</button>
+        <button
+          onClick={addCard}
+          className="w-full border border-primary rounded py-[11px] font-semibold text-primary mt-10 hover:bg-primary-200 hover:border-primary-200 duration-200"
+        >
+          + Add new link
+        </button>
 
-        <div className="bg-gray-300 mt-6 rounded py-[46px] px-5">
-          {isLinkAdded ?
-            <>
-              <LinkCard />
-            </>
-
-            :
-
-            <div className="max-w-[488px] mx-auto">
+        <div className="">
+          {cards.length > 0 ? (
+            <div className="">
+              {cards.map((id, index) => (
+                <LinkCard
+                  key={id}
+                  id={id}
+                  position={index + 1} // Pass position starting from 1
+                  onRemove={removeCard}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className="bg-gray-300 mt-6 rounded sm:py-[46px] px-5 mx-auto">
               <div>
-                <Image src={heroImage} width={240} height={160  } alt="hero image" className="mx-auto" />
+                <Image src={heroImage} width={240} height={160} alt="hero image" className="mx-auto" />
               </div>
 
               <h1 className="text-gray font-bold text-2xl md:text-3xl my-6 text-center">Let’s get you started</h1>
-              <p className="text-gray-100 text-center">Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!</p>
+              <p className="text-gray-100 text-center sm:max-w-[488px] mx-auto">Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!</p>
             </div>
-          }
+          )}
         </div>
-
-        <button className="w-full bg-button-base-active rounded py-[11px] font-semibold text-white mt-10">Save</button>
+        <div className="md:flex md:justify-end">
+          <button className="w-full md:w-[91px] bg-button-base rounded py-[11px] font-semibold text-white mt-10">Save</button>
+        </div>
       </div>
     </main>
   );
--- src/app/components/LinkCard.tsx
+++ src/app/components/LinkCard.tsx
@@ -1,9 +1,184 @@
-const LinkCard = () => {
-  return (
-    <div>
-      Link Card
-    </div>
-  )
+'use client';
+import React, { FC, useState } from 'react';
+import Image from 'next/image';
+
+import GithubIcon from '../../../public/assets/icon/icon-github.svg';
+import FrontendMentorIcon from '../../../public/assets/icon/icon-frontendmentor.svg';
+import TwitterIcon from '../../../public/assets/icon/icon-twitter.svg';
+import LinkedinIcon from '../../../public/assets/icon/icon-linkedin.svg';
+import YoutubeIcon from '../../../public/assets/icon/icon-youtube.svg';
+import FacebookIcon from '../../../public/assets/icon/icon-facebook.svg';
+import TwitchIcon from '../../../public/assets/icon/icon-twitch.svg';
+import DevtoIcon from '../../../public/assets/icon/icon-devto.svg';
+import CodewarsIcon from '../../../public/assets/icon/icon-codewars.svg';
+import CodepenIcon from '../../../public/assets/icon/icon-codepen.svg';
+import FreeCodeCampIcon from '../../../public/assets/icon/icon-freecodecamp.svg';
+import GitlabIcon from '../../../public/assets/icon/icon-gitlab.svg';
+import HashnodeIcon from '../../../public/assets/icon/icon-hashnode.svg';
+import StackOverFlowIcon from '../../../public/assets/icon/icon-stack-overflow.svg';
+
+type SocialMediaPlatform =
+  | 'Github'
+  | 'FrontendMentor'
+  | 'Twitter'
+  | 'Linkedin'
+  | 'Youtube'
+  | 'Facebook'
+  | 'Twitch'
+  | 'Devto'
+  | 'Codewars'
+  | 'Codepen'
+  | 'FreeCodeCamp'
+  | 'Gitlab'
+  | 'Hashnode'
+  | 'StackOverFlow';
+
+interface LinkCardProps {
+  id: number;
+  position: number;
+  onRemove: (id: number) => void;
 }
 
-export default LinkCard
+const socialMediaOptions: SocialMediaPlatform[] = [
+  'Github',
+  'FrontendMentor',
+  'Twitter',
+  'Linkedin',
+  'Youtube',
+  'Facebook',
+  'Twitch',
+  'Devto',
+  'Codewars',
+  'Codepen',
+  'FreeCodeCamp',
+  'Gitlab',
+  'Hashnode',
+  'StackOverFlow'
+];
+
+const socialMediaIcons: Record<SocialMediaPlatform, string> = {
+  Github: GithubIcon,
+  FrontendMentor: FrontendMentorIcon,
+  Twitter: TwitterIcon,
+  Linkedin: LinkedinIcon,
+  Youtube: YoutubeIcon,
+  Facebook: FacebookIcon,
+  Twitch: TwitchIcon,
+  Devto: DevtoIcon,
+  Codewars: CodewarsIcon,
+  Codepen: CodepenIcon,
+  FreeCodeCamp: FreeCodeCampIcon,
+  Gitlab: GitlabIcon,
+  Hashnode: HashnodeIcon,
+  StackOverFlow: StackOverFlowIcon,
+};
+
+const exampleLinks: Record<SocialMediaPlatform, string> = {
+  Github: 'https://github.com/username',
+  FrontendMentor: 'https://www.frontendmentor.io/profile/username',
+  Twitter: 'https://twitter.com/username',
+  Linkedin: 'https://www.linkedin.com/in/username',
+  Youtube: 'https://www.youtube.com/user/username',
+  Facebook: 'https://www.facebook.com/username',
+  Twitch: 'https://www.twitch.tv/username',
+  Devto: 'https://dev.to/username',
+  Codewars: 'https://www.codewars.com/users/username',
+  Codepen: 'https://codepen.io/username',
+  FreeCodeCamp: 'https://www.freecodecamp.org/username',
+  Gitlab: 'https://gitlab.com/username',
+  Hashnode: 'https://hashnode.com/@username',
+  StackOverFlow: 'https://stackoverflow.com/users/username'
+};
+
+const LinkCard: FC<LinkCardProps> = ({ id, position, onRemove }) => {
+  const [selectedSocialMedia, setSelectedSocialMedia] = useState<SocialMediaPlatform>('Github');
+  const [link, setLink] = useState('');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleRemove = () => {
+    onRemove(id);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleOptionClick = (option: SocialMediaPlatform) => {
+    setSelectedSocialMedia(option);
+    setLink(''); // Reset link when changing platform
+    setIsDropdownOpen(false);
+  };
+
+  return (
+    <div className="bg-gray-300 mt-6 rounded py-5 px-5 flex flex-col gap-4 text-gray-100">
+      <div className="flex items-center justify-between">
+        <h2 className="text-lg font-bold">Link #{position}</h2>
+        <button
+          onClick={handleRemove}
+          className="px-3 py-1 bg-red-500 rounded-md hover:bg-red-600"
+        >
+          Remove
+        </button>
+      </div>
+
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center p-3 border border-gray-300 rounded-md bg-white shadow-sm w-full text-left"
+        >
+          <Image
+            src={socialMediaIcons[selectedSocialMedia]}
+            alt={selectedSocialMedia}
+            width={18}
+            height={18}
+          />
+          <span className="ml-2">{selectedSocialMedia}</span>
+          <svg
+            className={`ml-auto w-4 h-4 transition-transform duration-200 ${isDropdownOpen ? 'rotate-180' : 'rotate-0'}`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+          </svg>
+        </button>
+
+        {isDropdownOpen && (
+          <div className="absolute z-10 mt-2 w-full p-6 bg-white border border-gray-300 rounded-md shadow-lg">
+            {socialMediaOptions.map((option) => (
+              <button
+                key={option}
+                onClick={() => handleOptionClick(option)}
+                className="flex items-center w-full px-4 py-3 hover:bg-gray-300 text-left border-b border-gray-200 last:border-b-0"
+              >
+                <Image src={socialMediaIcons[option]} alt={option} width={18} height={18} />
+                <span className="ml-2">{option}</span>
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="relative flex items-center">
+        <Image
+          src="/assets/icon/icon-link.svg"
+          alt="Link"
+          width={12}
+          height={12}
+          className="absolute left-5 top-6"
+        />
+        <input
+          id={`link-${id}`}
+          type="text"
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+          placeholder={`e.g ${exampleLinks[selectedSocialMedia]}`}
+          className="mt-1 block w-full p-3 border border-gray-200 rounded focus:shadow-input outline-none pl-10"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LinkCard;
